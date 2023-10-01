49ae6f16f82fd2d30d54ff0e0660fdc04ebba2d8
tiago sampaio cordeiro <sampaio415@gmail.com>
2023-10-01 20:23:03 -0300

    segundo teste

--- tests/Unit/teste-atividade.php
+++ tests/Unit/teste-atividade.php
@@ -4,18 +4,11 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class calculadora extends TestCase
-
+final class EqualsTest extends TestCase
 {
-
-    function somar($numero1, $numero2) {
-        $numero1 = 5;
-        $numero2 = 5;
-        $resultado = $numero1 + $numero2;
-        return $resultado;
-        $this->assertSame(5, $resultado);
+    public function testFailure(): void
+    {
+        $this->assertEquals(1, 0);
     }
 
 }
-
-
