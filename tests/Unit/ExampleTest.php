6fe4b253d86f99508d4a81d2eec393eb001441b4
tiago sampaio cordeiro <sampaio415@gmail.com>
2023-10-01 22:45:51 -0300

    gitgit

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -14,3 +14,24 @@ class ExampleTest extends TestCase
         $this->assertTrue(true);
     }
 }
+
+
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class testAtividade extends TestCase
+{
+    /**
+     * A basic test example.
+     */
+    public function soma(): void
+    {
+        $number1 = 5;
+        $number2 = 5;
+        $resultado = $number1 + $number2;
+
+        $this->assertSame(15, $resultado);
+    }
+}
