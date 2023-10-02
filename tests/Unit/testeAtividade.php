3f2358120b8bd3bd96072c8730a45e04faf8cb5c
tiago sampaio cordeiro <sampaio415@gmail.com>
2023-10-01 21:35:16 -0300

    teste quarto

--- tests/Unit/testeAtividade.php
+++ tests/Unit/testeAtividade.php
@@ -1,15 +1,11 @@
 <?php
 
-namespace Tests\Unit;
+namespace testAtividade;
 
 use PHPUnit\Framework\TestCase;
 
 class testeAtividade extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-
     public function testFailure(): void
     {
         $this->assertSame('2204', '220');
