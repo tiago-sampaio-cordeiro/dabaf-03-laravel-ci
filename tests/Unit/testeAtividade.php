45d55eb45ebe7b9cbf07eeb6ee07bb59c9f208e6
tiago sampaio cordeiro <sampaio415@gmail.com>
2023-10-02 18:51:50 -0300

    testeAtividade

--- tests/Unit/testeAtividade.php
+++ tests/Unit/testeAtividade.php
@@ -1,27 +1,14 @@
+
 <?php
 
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class testeAtividade extends TestCase
+final class EqualsTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    function somar($numero1, $numero2) {
-        return $numero1 + $numero2;
-    
-    
-    // Exemplo de uso da função
-    $numero1 = 5;
-    $numero2 = 5;
-    $resultado = somar($numero1, $numero2);
-   
+    public function testFailure(): void
     {
-        $this->assertEquals(15,$resultado);
+        $this->assertEquals(1, 0);
     }
 }
-}
-
-
