3d9a8a82e66b0025c2ab7c5284406da8feb0cac9
tiago sampaio cordeiro <sampaio415@gmail.com>
2023-10-04 18:10:55 -0300

    AtividadeTest

--- tests/Unit/AtividadeTest.php
+++ tests/Unit/AtividadeTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use PhpParser\Node\Stmt\Echo_;
 use PHPUnit\Framework\TestCase;
 
 final class AtividadeTest extends TestCase
@@ -11,7 +12,21 @@ final class AtividadeTest extends TestCase
         $num1 = 10;
         $num2 = 15;
         $resultado = $num1 + $num2;
-        
+
         $this->assertEquals(10, $resultado);
+    }
+
+    public function idade() {
+        $anoNascimento = 1960;
+        $anoAtual = date('Y');
+        $idade = $anoAtual - $anoNascimento;
+        $this->assertSame(35, $idade);
+    }
+
+    public function desconto() {
+        $preco = 100;
+        $desconto = 10;
+        $precoFinal = $preco - ($preco * ($desconto/100));
+        $this->assertSame(80, $precoFinal);
     }
 }
