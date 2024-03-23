8f227f64ec2d706f9cb914600641e99bb7a14a7b
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-03-23 20:37:35 -0300

    teste de simbolo @

---
+++ tests/Unit/SymbolTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SymbolTest extends TestCase
+{
+    
+    public function test_Contain_@_In_Email(): void
+    {
+        $email = 'usuario@example.com';
+
+        $this->assertStringContainsString('@', $email);
+    }
+}
