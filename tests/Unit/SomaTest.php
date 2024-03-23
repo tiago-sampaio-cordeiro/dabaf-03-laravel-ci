fbe8330c497cfb38d363af1c4192a01ee02c3f09
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-03-23 20:10:53 -0300

    teste de soma

---
+++ tests/Unit/SomaTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class SomaTest extends TestCase
+{
+    public function test_Sum_Of_Two_Numbers(): void
+    {
+        $numero1 = 10;
+        $numero2 = 20;
+        $resultado = $numero1 + $numero2;
+
+
+        $this->assertEquals(40, $resultado);
+    }
+}
