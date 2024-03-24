6772a3331931d560b09fe63a4bf6661bec5887e6
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-03-24 15:32:54 -0300

    teste de formato 5

--- tests/Unit/SymbolTest.php
+++ tests/Unit/SymbolTest.php
@@ -11,8 +11,8 @@ class SymbolTest extends TestCase
     {
         $email = "usuario@email.com";
 
-        $format_email = "%s@%s.%s";
+        $format_email = "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
 
-        $this->assertMatchesRegularExpression(sprintf($formatoEmail, '[a-zA-Z0-9._%+-]+', '[a-zA-Z0-9.-]+', '[a-zA-Z]{2,}'), $email);
+        $this->assertMatchesRegularExpression($format_email , $email);
     }
 }
