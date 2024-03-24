7bf027e50cb56f6aee06814d0ab6d9b50f444714
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2024-03-24 14:34:16 -0300

    teste de formato 6

--- tests/Unit/SymbolTest.php
+++ tests/Unit/SymbolTest.php
@@ -11,6 +11,6 @@ class SymbolTest extends TestCase
     {
         $email = "usuario@email.com";
 
-        $this->assertStringMatchesFormatFile('~/example-app/email/format_email', '$email');
+        $this->assertStringMatchesFormatFile('~/example-app/email/format_email.txt', '$email');
     }
 }
